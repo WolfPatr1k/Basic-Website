c928d64ecd65f0c293649bd826a8fa9e3871113f
WolfPatr1k <patrik.veres1107@gmail.com>
2023-08-29 15:28:29 +0200

    Working date and time

--- scripts/dateTimeIp.js
+++ scripts/dateTimeIp.js
@@ -1,7 +1,9 @@
-const d = new Date();
+setInterval(showDateTime, 1000);
 
 function showDateTime() {
-    document.getElementById('Date').innerHTML = String(d.getDate())+"."+String(d.getMonth()+1)+"."+String(d.getFullYear());
+    var d = new Date();
+    document.getElementById('Date').innerHTML = "Mai d\u00e1tum: " + String(d.getDate()).padStart(2, '0') + "." + String(d.getMonth() + 1).padStart(2, '0') + "." + String(d.getFullYear());
+    document.getElementById('Time').innerHTML = "Pontos id\u0151: " + String(d.getHours()).padStart(2, '0') + ":" + String(d.getMinutes()).padStart(2, '0');
 }
 
 window.onload = showDateTime;
