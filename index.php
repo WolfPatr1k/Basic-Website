6f4f6232b0804e3eeb9e54c635a9786e7f4ab93d
WolfPatr1k <patrik.veres1107@gmail.com>
2023-08-29 19:24:22 +0200

    idk

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+readfile('html/main.html');
+?>
+
+<style>
+<?php readfile('css/stylesheet.css'); ?>
+</style>
--- scripts/dateTimeIp.js
+++
@@ -1,9 +0,0 @@
-setInterval(showDateTime, 1000);
-
-function showDateTime() {
-    var d = new Date();
-    document.getElementById('Date').innerHTML = "Mai d\u00e1tum: " + String(d.getDate()).padStart(2, '0') + "." + String(d.getMonth() + 1).padStart(2, '0') + "." + String(d.getFullYear());
-    document.getElementById('Time').innerHTML = "Pontos id\u0151: " + String(d.getHours()).padStart(2, '0') + ":" + String(d.getMinutes()).padStart(2, '0');
-}
-
-window.onload = showDateTime;
